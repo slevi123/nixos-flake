8f37eb85e696e0f1152c54e67bb0c081967f6856
Simofi Levente <simofilevente@gmail.com>
2023-10-14 21:22:40 +0300

    generated flake.lock

--- flake.nix
+++ flake.nix
@@ -1,12 +1,9 @@
 {
-  inputs.nixpkgs.url = github:NixOS/nixpkgs;
-  # inputs.home-manager.url = github:nix-community/home-manager;
-
-  outputs = { self, nixpkgs, ... }@attrs: {
-    nixosConfigurations.fnord = nixpkgs.lib.nixosSystem {
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations.leswell-nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      specialArgs = attrs;
       modules = [ ./configuration.nix ];
     };
   };
 }
+
