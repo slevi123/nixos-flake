ed788400365fce1d282544d70d345eca8b9b0de8
leswell <simofilevente+git@gmail.com>
2025-01-27 00:39:20 +0200

    moved gnome extensions into home-manager option

--- home-manager/packages.nix
+++ home-manager/packages.nix
@@ -9,36 +9,6 @@
         idea-ultimate
         clion
 
-    ]) ++ (with pkgs.gnomeExtensions; [
-        clipboard-history
-        astra-monitor
-        media-controls
-        emoji-copy
-        blur-my-shell
-        burn-my-windows
-        autohide-battery
-        # caffeine
-        # bubblemail
-        xwayland-indicator
-
-        # gnomeExtensions.noannoyance-2
-        
-        # system-monitor
-
-        # pop-shell  # buggy, shortcuts not working
-        # forge
-
-        # gnomeExtensions.gsconnect  # not working
-        # switcher # I got ulauncher
-
-        # power-profile-switcher
-        # power-profile-indicator  # not compatible
-
-        # ideapad
-        # ideapad-controls # may try in future
-        
-        # auto-cpufreq # not installing
-        # cpufreq
     ]) ++ [
          (let base = pkgs.appimageTools.defaultFhsEnvArgs; in 
             pkgs.buildFHSUserEnv (base // {
