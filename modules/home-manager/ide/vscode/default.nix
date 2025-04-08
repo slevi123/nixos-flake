c8bbdcb9e912c10fea603af47a3fb818ef64de91
leswell <simofilevente+git@gmail.com>
2025-04-08 14:03:54 +0300

    options renamed

--- modules/home-manager/ide/vscode/default.nix
+++ modules/home-manager/ide/vscode/default.nix
@@ -1,30 +1,9 @@
 {pkgs, ...}: {
+  imports = [
+    ./profiles/default-profile.nix
+  ];
+
   programs.vscode = {
     enable = true;
-    extensions = with pkgs.vscode-extensions; [
-      k--kato.intellij-idea-keybindings
-      mkhl.direnv
-      mhutchie.git-graph
-
-      # language support
-      bbenoist.nix
-      tamasfe.even-better-toml
-      # kdl-org.kdl
-
-
-      gruntfuggly.todo-tree
-      pkief.material-icon-theme
-
-      naumovs.color-highlight
-      # ewen-lbh.vscode-hyprls # not added yet to nixpkgs
-    ];
-    userSettings = {
-      "editor.fontFamily" = "'Hack Nerd Font Mono', 'monospace', monospace";
-      "files.autoSave" = "afterDelay";
-      "workbench.colorTheme" = "Wal";
-      "workbench.iconTheme" = "material-icon-theme";
-      "window.menuBarVisibility" = "toggle";
-      "editor.scrollbar.vertical" = "hidden";
-    };
   };
 }
