bc86ca3f9817ef72bcaea7cf41a45d8bf6a0e1a1
leswell <simofilevente+git@gmail.com>
2025-06-11 16:41:06 +0300

    added pwa support for firefox

--- modules/home-manager/program/browser/firefox/default.nix
+++ modules/home-manager/program/browser/firefox/default.nix
@@ -2,5 +2,12 @@
 {
   home.packages = with pkgs; [
     firefox
+    firefoxpwa
   ];
+
+  programs.firefox = {
+    enable = true;
+    package = pkgs.firefox;
+    nativeMessagingHosts = [ pkgs.firefoxpwa ];
+  };
 }
