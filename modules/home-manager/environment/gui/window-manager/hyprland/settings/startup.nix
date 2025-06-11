a830727d85556824addf06ef1905e1e3d74ad206
leswell <simofilevente+git@gmail.com>
2025-06-11 16:58:03 +0300

    waybar changes (added soundcloud)

--- modules/home-manager/environment/gui/window-manager/hyprland/settings/startup.nix
+++ modules/home-manager/environment/gui/window-manager/hyprland/settings/startup.nix
@@ -4,14 +4,16 @@
     exec-once = [
       "systemctl --user start hyprpolkitagent"
       "hyprsunset -t 5000"
-      "swaync & avizo-service"
+      "swaync & avizo-service &"
       "udiskie --smart-tray &"
-      "$CHARM_WALLPAPER &"
       "$CHARM_BAR &"
-      "$CHARM_IDLE"
-      "clipse -listen"
+      "$CHARM_IDLE &"
+      "clipse -listen &"
+      "$CHARM_WALLPAPER &"
       "~/.charm/scripts/hyprland/custom-ipc-handler.sh"
-      "echo hehe > /tmp/kortekiraly" 
+    ];
+
+    execr-once = [
     ];
   };
 }
