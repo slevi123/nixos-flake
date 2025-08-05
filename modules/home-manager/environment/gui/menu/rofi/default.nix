86b463377b85f86e923d706fbe68b27316fe3485
leswell <simofilevente+git@gmail.com>
2025-08-05 22:55:10 +0300

    fixme added for rofi

--- modules/home-manager/environment/gui/menu/rofi/default.nix
+++ modules/home-manager/environment/gui/menu/rofi/default.nix
@@ -3,7 +3,7 @@
   programs.rofi = {
     enable = true;
     package = pkgs.rofi-wayland;
-
+    # FIXME: rofi config is not here, so it's not declarative now!
     # FIXME: create input for themes and copy them into theme-selector
     theme = "/home/leswellhm/.local/share/rofi/themes/rounded-red-dark.rasi"; 
     location = "center";
