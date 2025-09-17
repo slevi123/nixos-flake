6d0377b8c903d1254c5e1bb1a2ad7589aab02dcf
leswell <simofilevente+git@gmail.com>
2025-09-17 23:21:03 +0300

    app starts with uwsm now

--- modules/home-manager/environment/gui/window-manager/hyprland/settings/keymaps/programs-basic.nix
+++ modules/home-manager/environment/gui/window-manager/hyprland/settings/keymaps/programs-basic.nix
@@ -4,26 +4,26 @@
     "$terminal" = "kitty";
     # "$menu" = "ulauncher-toggle";
     bind = [
-      "SUPER, Q, exec, $terminal"
-      "SUPER, Z, exec, zen"
+      "SUPER, Q, exec, uwsm app -- $terminal"
+      "SUPER, Z, exec, uwsm app zen"
       "SUPER, C, killactive,"
       "SUPER, M, exec, uwsm stop"
       "SUPER, f, fullscreen"
       "SUPER SHIFT, f, togglefloating,"
       # "SUPER, R, exec, wofi --show=drun --height=70% --width=40%"
-      "SUPER, R, exec, rofi -show drun -show-icons"
+      "SUPER, R, exec, uwsm app -- rofi -show drun -show-icons -run-command 'uwsm app -- {cmd}'"
 
-      "SUPER SHIFT, W, exec, ~/.config/waybar/launch.sh # Reload waybar"
-      "SUPER CTRL, W, exec, ~/.config/waybar/toggle.sh # Toggle waybar"
-      "SUPER SHIFT, A, exec, ~/.config/ashell/toggle.sh # Toggle ashell"
-      "SUPER, L, exec, hyprlock"
-      "SUPER, Space, exec, ~/.charm/scripts/hyprland/switch-lang.sh # Switch language"
-      "SUPER SHIFT, B, exec, ~/.charm/scripts/hyprland/toggle-hyprsunset.sh # Toggle bluelight (hyprsunset)"
-      "SUPER, T, exec, kitten quick-access-terminal"
+      "SUPER SHIFT, W, exec, uwsm app ~/.config/waybar/launch.sh # Reload waybar"
+      "SUPER CTRL, W, exec, uwsm app ~/.config/waybar/toggle.sh # Toggle waybar"
+      "SUPER SHIFT, A, exec, uwsm app ~/.config/ashell/toggle.sh # Toggle ashell"
+      "SUPER, L, exec, uwsm app hyprlock"
+      "SUPER, Space, exec, uwsm app ~/.charm/scripts/hyprland/switch-lang.sh # Switch language"
+      "SUPER SHIFT, B, exec, uwsm app ~/.charm/scripts/hyprland/toggle-hyprsunset.sh # Toggle bluelight (hyprsunset)"
+      "SUPER, T, exec, uwsm app kitten quick-access-terminal"
     ];
 
     bindd = [
-      "SUPER, P, Opens up quick-actions menu, exec, quick-actions"
+      "SUPER, P, Opens up quick-actions menu, exec, uwsm app quick-actions"
     ];
   };
 }
--- modules/home-manager/environment/gui/window-manager/hyprland/settings/startup.nix
+++ modules/home-manager/environment/gui/window-manager/hyprland/settings/startup.nix
@@ -3,15 +3,16 @@
   wayland.windowManager.hyprland.settings = {
     exec-once = [
       "systemctl --user start hyprpolkitagent"
-      "hyprsunset -t 5000"
-      "swaync & avizo-service"
-      "udiskie --smart-tray"
+      "uwsm app hyprsunset -t 5000"
+      "uwsm app swaync"
+      "uwsm app avizo-service"
+      "uwsm app -- udiskie --smart-tray"
       "uwsm app -- $CHARM_BAR"
-      "$CHARM_IDLE &"
-      "clipse -listen"
-      "hyprpaper"
-      "~/.charm/scripts/hyprland/custom-ipc-handler.sh"
-      "battery-notify"
+      "uwsm app -- $CHARM_IDLE"
+      "uwsm app -- clipse -listen"
+      "uwsm app hyprpaper"
+      "uwsm app ~/.charm/scripts/hyprland/custom-ipc-handler.sh"
+      "RUST_LOG=none uwsm app battery-notify"
     ];
 
     execr-once = [
