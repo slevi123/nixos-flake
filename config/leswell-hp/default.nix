aa2056a70d93ab7ec31b6737c8aa9d14426b18de
leswell <simofilevente+git@gmail.com>
2025-10-20 02:39:30 +0300

    first try -15

--- config/leswell-hp/default.nix
+++ config/leswell-hp/default.nix
@@ -27,24 +27,24 @@ nixpkgs.lib.nixosSystem {
     "${self}/bits/nixos/users/groups.nix"
     "${self}/bits/nixos/users/leswellhm.nix"
     # inputs.hyprland.nixosModules.default
-    {
-      home-manager = {
-        users.leswellhm = ../../homes/parts/charm-de.nix;
+    # {
+    #   home-manager = {
+    #     users.leswellhm = ../../homes/parts/charm-de.nix;
 
-        extraSpecialArgs = {
-          inherit inputs self;
-          stateVersion = "25.05";
-        };
-        useGlobalPkgs = true;
-        useUserPackages = true;
-        sharedModules = [
-          # modules shared between all users
-          # "${self}/bits/home-manager/environment/gui/desktop-environment/gnome"
-          # (import "${self}/homes/parts/common.nix" (args // { stateVersion = "23.05"; }))
-          "${self}/homes/parts/common.nix"
-        ];
-        backupFileExtension = "hm-backup-g2";
-      };
-    }
+    #     extraSpecialArgs = {
+    #       inherit inputs self;
+    #       stateVersion = "25.05";
+    #     };
+    #     useGlobalPkgs = true;
+    #     useUserPackages = true;
+    #     sharedModules = [
+    #       # modules shared between all users
+    #       # "${self}/bits/home-manager/environment/gui/desktop-environment/gnome"
+    #       # (import "${self}/homes/parts/common.nix" (args // { stateVersion = "23.05"; }))
+    #       "${self}/homes/parts/common.nix"
+    #     ];
+    #     backupFileExtension = "hm-backup-g2";
+    #   };
+    # }
   ];
 }
