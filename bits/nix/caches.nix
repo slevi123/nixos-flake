5a2cbea9e954ae3950aa7cb72522d529f4f6575b
leswell <simofilevente+git@gmail.com>
2025-10-21 16:52:23 +0300

    github workflows added 6 - cachix

--- bits/nix/caches.nix
+++ bits/nix/caches.nix
@@ -1,7 +1,16 @@
 {
   nix.settings = {
-    substituters = [ "https://nix-community.cachix.org" ];
-    trusted-substituters = [ "https://nix-community.cachix.org" ];
-    trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
+    substituters = [
+      "https://nix-community.cachix.org"
+      "https://charm-os.cachix.org"
+    ];
+    trusted-substituters = [
+      "https://nix-community.cachix.org"
+      "https://charm-os.cachix.org"
+    ];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "charm-os.cachix.org-1:i3YCtwuNr+OjEoZnzUp6iCSvxFu2ksXuoKZnCzOt5ks="
+    ];
   };
 }
