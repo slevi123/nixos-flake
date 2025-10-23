466aeba1367fb69c053586c9bfcad44edffda7b8
leswell <simofilevente+git@gmail.com>
2025-10-24 01:09:56 +0300

    feat: ssh-agent timeout set to 1h

--- bits/nixos/environment/remote/ssh/default.nix
+++ bits/nixos/environment/remote/ssh/default.nix
@@ -1,4 +1,7 @@
 {
   # to be able to use `ssh-add <KEY_PATH>` to cache key passwd until user session, `ssh-add -l` to list 'em
-  programs.ssh.startAgent = true;
+  programs.ssh = {
+    startAgent = true;
+    agentTimeout = "1h";
+  };
 }
