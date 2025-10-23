431972d2ac270248d5fa97ba4c0c397724d1520a
leswell <simofilevente+git@gmail.com>
2025-10-24 01:37:00 +0300

    feat: ssh-askpassword enabled

--- bits/nixos/environment/remote/ssh/default.nix
+++ bits/nixos/environment/remote/ssh/default.nix
@@ -1,7 +1,11 @@
+{ config, ... }:
 {
   # to be able to use `ssh-add <KEY_PATH>` to cache key passwd until user session, `ssh-add -l` to list 'em
   programs.ssh = {
     startAgent = true;
     agentTimeout = "1h";
+
+    # x11 is required for it 
+    enableAskPassword =  config.programs.hyprland.xwayland.enable;
   };
 }
