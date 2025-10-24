16b38200cb2011abc55b6b2544e2083003b04448
leswell <simofilevente+git@gmail.com>
2025-10-24 15:42:34 +0300

    more refactor based rleated to `"${system}"` usage #16

--- bits/home-manager/ide/vscode/profiles/globals/extension_repos.nix
+++ bits/home-manager/ide/vscode/profiles/globals/extension_repos.nix
@@ -1,6 +1,8 @@
 { pkgs, inputs, system }:
 {
   nixpkgs = pkgs.vscode-extensions;
+  
+  # must use system [wrong format ...]
   community = inputs.nix-vscode-extensions.extensions.${system};
   inherit (pkgs) nix4vscode;
 }
