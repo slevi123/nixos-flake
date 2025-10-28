76b7f6e7ca7f35552149bbf29da647c2a67670eb
leswell <simofilevente+git@gmail.com>
2025-10-28 20:53:05 +0200

    plymouth-added

---
+++ bits/nixos/bootloader/plymouth/default.nix
@@ -0,0 +1,10 @@
+{self, pkgs, ...}: {
+  boot.plymouth = {
+    enable = true;
+    theme = "glow";
+    logo = "${self}/media/CharmOSLogo/ChatGPT-Image_iter2.png";
+    # themePackages = [
+    #   pkgs.adi1090x-plymouth-themes
+    # ];
+  };
+}
