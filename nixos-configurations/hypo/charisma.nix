e3e783bbcd844abcf30733d8e603b0c4568cda4a
leswell <simofilevente+git@gmail.com>
2025-10-30 19:10:53 +0200

    transition to EZConfig (with flske-parts) 3

--- nixos-configurations/hypo/charisma.nix
+++ nixos-configurations/hypo/charisma.nix
@@ -2,5 +2,5 @@
   system.stateVersion = "25.05";
   # hostName = "HyPo";
   boot.loader.grub.efiSupport = false;
-  flake-path = "/my_own_nixos/nixos-flake";
+  programs.nh.flake = "/my_own_nixos/nixos-flake";
 }
